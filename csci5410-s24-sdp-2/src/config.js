29d95e76b4d9febf999b6a4bfc85cca983007201
Shreya Kapoor <31164665+shreyakapoor08@users.noreply.github.com>
2024-08-24 05:04:29 -0300

    Update config.js

--- csci5410-s24-sdp-2/src/config.js
+++ csci5410-s24-sdp-2/src/config.js
@@ -4,16 +4,16 @@ import { getFirestore } from "firebase/firestore";
 export const userPoolId = "us-east-1_PpZiaKLpu"
 
 export const clientId= "7npnk7bc9gain1t9g4a4956atp"
- 
-const firebaseConfig = {
-    apiKey: "AIzaSyARpxqAeAVeH_EyC6V3VPkwSVZhE9rNjTo",
-    authDomain: "dalvacationhome-sdp-2.firebaseapp.com",
-    projectId: "dalvacationhome-sdp-2",
-    storageBucket: "dalvacationhome-sdp-2.appspot.com",
-    messagingSenderId: "521114221538",
-    appId: "1:521114221538:web:139704d42c7436900757bb",
-    measurementId: "G-MGXCF07BVC"
-};
+ //uncomment before using
+// const firebaseConfig = {
+//     apiKey: "AIzaSyARpxqAeAVeH_EyC6V3VPkwSVZhE9rNjTo",
+//     authDomain: "dalvacationhome-sdp-2.firebaseapp.com",
+//     projectId: "dalvacationhome-sdp-2",
+//     storageBucket: "dalvacationhome-sdp-2.appspot.com",
+//     messagingSenderId: "521114221538",
+//     appId: "1:521114221538:web:139704d42c7436900757bb",
+//     measurementId: "G-MGXCF07BVC"
+// };
  
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
